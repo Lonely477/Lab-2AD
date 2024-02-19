13c2c451572ad8c1dedf03d2844dbca97e564de7
filip <128900467+Lonely477@users.noreply.github.com>
2024-02-19 22:07:38 +0100

    Api Controller for Books

---
+++ LibApp/Controllers/Api/ApiBooksController.cs
@@ -0,0 +1,34 @@
+﻿using LibApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibApp.Controllers.Api
+{
+    public class ApiBooksController : ControllerBase
+    {
+        private readonly IHttpClientFactory ClientFactory;
+
+
+
+        public ApiBooksController(IHttpClientFactory httpClientFactory)
+        {
+            ClientFactory = httpClientFactory;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+
+            var client = ClientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://localhost:7192/Controllers/api/booksapi");
+            response.EnsureSuccessStatusCode();
+
+            var books = await response.Content.ReadFromJsonAsync<IEnumerable<Book>>();
+
+
+            return Ok(books);
+        }
+    }
+}
