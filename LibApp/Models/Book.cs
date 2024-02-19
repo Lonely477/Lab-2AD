422126e63615d4d21829dc87876a9208a3191f9c
filip <128900467+Lonely477@users.noreply.github.com>
2024-02-19 21:30:48 +0100

    book validation

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -1,14 +1,21 @@
-﻿namespace LibApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public string Title { get; set; }
         public string Author { get; set; }
         public Genre Genre { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public byte GenreId { get; set; }
         public DateTime DateAdded { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
         public DateTime? ReleaseDate { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Range(1, 20, ErrorMessage = "NumberInStock field must be between 1 and 20.")]
         public int NumberInStock { get; set; }
     }
 }
